ee59eabf99eae4dc8f43a97f643eb511a8032f98
natanael.santos <natanaelsantoss2br@gmail.com>
2024-09-04 09:57:46 -0300

    removendo ping

--- src/main/java/br/ufpb/dcx/rodrigor/servico/ping/controllers/PingController.java
+++ src/main/java/br/ufpb/dcx/rodrigor/servico/ping/controllers/PingController.java
@@ -1,7 +1,6 @@
 package br.ufpb.dcx.rodrigor.servico.ping.controllers;
 
 import br.ufpb.dcx.rodrigor.servico.Keys;
-import br.ufpb.dcx.rodrigor.servico.login.LoginController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.http.Context;
 import org.apache.logging.log4j.LogManager;
@@ -11,7 +10,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
